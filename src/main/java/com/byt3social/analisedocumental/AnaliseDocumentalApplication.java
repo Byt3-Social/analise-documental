04594beb56b2e0e5d48ff36ff5bae9ecfb7abde4
Leandro Pereira <leojgpereira@gmail.com>
2023-09-20 01:16:32 -0400

    Commit inicial

---
+++ src/main/java/com/byt3social/analisedocumental/AnaliseDocumentalApplication.java
@@ -0,0 +1,13 @@
+package com.byt3social.analisedocumental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDocumentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDocumentalApplication.class, args);
+	}
+
+}
